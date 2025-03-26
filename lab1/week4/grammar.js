d6c8e75514b50bee6013cd492a32bcc88269d7c6
余尧辰 <2324923940@qq.com>
2025-03-26 09:09:14 +0000

    update lab1/week4/grammar.js. 添加了最基础的grammar.js内容
    Signed-off-by: 余尧辰 <2324923940@qq.com>

---
+++ lab1/week4/grammar.js
@@ -0,0 +1,193 @@
+module.exports = grammar({
+    name: 'typescript',
+    extras: $ => [
+        $.comment,
+        /\s/,  // Whitespace
+        /[\s\p{Zs}\uFEFF\u2028\u2029\u2060\u200B]/,
+    ],
+    // precedences: $ => [
+
+    //     ['declaration', 'literal'],
+
+    // ],
+    word: $ => $.identifier,
+    rules: {
+        program: $ => seq(
+          optional($.hash_bang_line),
+          repeat($.statement),
+        ),
+    
+        hash_bang_line: _ => /#!.*/,
+
+        statement: $ => choice(
+            $.declaration,
+            $.statement_block,
+            $.expression_statement,
+            $.statement_block,
+            $.if_statement,
+            $.for_statement
+        ),
+
+        for_statement: $ => seq(
+            // week4 for语句
+        ),
+
+        binary_expression: $ => choice(
+      ...[
+        ['&&', 'logical_and'],
+        ['||', 'logical_or'],
+        ['>>', 'binary_shift'],
+        ['>>>', 'binary_shift'],
+        ['<<', 'binary_shift'],
+        ['&', 'bitwise_and'],
+        ['^', 'bitwise_xor'],
+        ['|', 'bitwise_or'],
+        ['+', 'binary_plus'],
+        ['-', 'binary_plus'],
+        ['*', 'binary_times'],
+        ['/', 'binary_times'],
+        ['%', 'binary_times'],
+        ['**', 'binary_exp', 'right'],
+        ['<', 'binary_relation'],
+        ['<=', 'binary_relation'],
+        ['==', 'binary_equality'],
+        ['===', 'binary_equality'],
+        ['!=', 'binary_equality'],
+        ['!==', 'binary_equality'],
+        ['>=', 'binary_relation'],
+        ['>', 'binary_relation'],
+        ['??', 'ternary'],
+        ['instanceof', 'binary_relation'],
+        ['in', 'binary_relation'],
+      ].map(([operator, precedence, associativity]) =>
+        (associativity === 'right' ? prec.right : prec.left)(precedence, seq(
+          field('left', operator === 'in' ? choice($.expression, $.private_property_identifier) : $.expression),
+          field('operator', operator),
+          field('right', $.expression),
+        )),
+      ),
+    ),
+
+
+        if_statement: $ => prec.right(seq(
+            //week3 if语句
+        )),
+       
+        parenthesized_expression: $ => seq(
+            //week3 括号表达式,勇于if_statement的条件部分
+        ),
+
+        variable_declaration: $ => seq(
+            //week3 变量声明
+        ),
+
+       
+
+        number: _ => {
+          //week2任务，10、16进制数的正则表达式
+          const hex_literal = ;
+    
+          const decimal_digits = ;
+    
+          return token(choice(
+            hex_literal,
+            decimal_digits,
+          ));
+        },
+        expression_statement: $ => seq(
+            $.expression,
+            optional($._semicolon),
+        ),
+        expression: $ => choice(
+            $.assignment_expression,
+            $.identifier,
+            $.number,
+            $.binary_expression
+        ),
+
+        assignment_expression: $ => prec.right(seq(
+            optional('using'),
+            field('left', $.identifier),
+            '=',
+            field('right', $.expression),
+        )),
+
+        declaration: $ => choice(
+            $.function_declaration,
+            $.variable_declaration,
+        ),
+
+        function_declaration: $ => prec.right(seq(
+            optional('async'),
+            'function',
+            field('name', $.identifier),
+            $.call_signature,
+            field('body', $.statement_block),
+            optional($._semicolon),
+          )),
+        
+        call_signature: $ => seq(
+            //week2任务，函数的调用签名，包括参数与返回类型
+          
+        ),
+        formal_parameters: $ => seq(
+            '(',
+            //week2
+            ')',
+        ),
+        required_parameter: $ => seq(
+            $.identifier,
+            field('type', optional($.type_annotation)),
+        ),
+
+
+        type_annotation: $ => seq(
+            ':',$.primitive_type,
+        ),
+
+        primitive_type: _ => choice(
+            'any', 'number','boolean','string','symbol','void','unknown','string','never','object',
+        ),
+        statement_block: $ => prec.right(seq(
+            '{',
+            repeat($.statement),
+            '}',
+            optional($._semicolon),
+        )),
+
+          
+        comment: $ => choice(
+            token(choice(
+            seq('//', /.*/),
+            seq(
+                '/*',
+                /[^*]*\*+([^/*][^*]*\*+)*/,
+                '/',
+            ),
+            )),
+        ),
+
+        
+        
+        identifier: $ => /[_a-zA-Z][_a-zA-Z0-9]*/, 
+        
+        // 分号
+        _semicolon: $ => ';',
+
+    }
+    
+})
+
+
+
+function commaSep1(rule) {
+    return seq(rule, repeat(seq(',', rule)));
+  }
+  
+  function commaSep(rule) {
+    return optional(commaSep1(rule));
+  }
+  
+  function sepBy1(sep, rule) {
+    return seq(rule, repeat(seq(sep, rule)));
+  }
