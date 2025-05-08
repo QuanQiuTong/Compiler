88d9541d9f12b1fb1e693c0ed72f9e106914e582
QuanQiuTong <fqt1550887523@outlook.com>
2025-05-08 10:11:00 +0800

    doc: AST, case 2 and Java case of `lab2/week2`

---
+++ lab2/week2/case.java
@@ -0,0 +1,5 @@
+public static void main(String[] args){
+    a = b;
+    c = d;
+    e = 500;
+}
---
+++ lab2/week2/case2.ts
@@ -0,0 +1,7 @@
+ function foo (x:number = 1): void {
+    a = b
+    c = d
+    c[1] = d
+    // c.x = d
+    e = 500
+}
